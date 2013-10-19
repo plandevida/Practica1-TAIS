2f8c88bf2561db0b6415cc3ea6b2d0a72ef74a0c
Plandevida <dserrano@MacBook-Pro-de-Daniel.local>
2013-10-19 22:19:17 +0200

    Proyecto

---
+++ Practica1-TAIS/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Practica1-TAIS
+//
+//  Created by Daniel on 19/10/13.
+//  Copyright (c) 2013 Daniel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
